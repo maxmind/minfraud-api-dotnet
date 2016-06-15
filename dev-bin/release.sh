7ff6ac43f7936e6b5a133da669ca6a38854ec6ed
Gregory Oschwald <goschwald@maxmind.com>
2016-06-15 09:12:08 -0700

    Update release script

--- dev-bin/release.sh
+++ dev-bin/release.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-set -e
+set -eu -o pipefail
+
+shopt -s extglob
+
+PROJECT_JSON="MaxMind.MinFraud/project.json"
+
 
 VERSION=$(perl -MFile::Slurp::Tiny=read_file -MDateTime <<EOF
 use v5.16;
@@ -15,18 +20,59 @@ EOF
 
 TAG="v$VERSION"
 
+
 if [ -n "$(git status --porcelain)" ]; then
     echo ". is not clean." >&2
+    exit 1
+fi
+
+jq ".version=\"$VERSION\"" "$PROJECT_JSON"| sponge "$PROJECT_JSON"
+
+git diff
+
+read -e -p "Continue (and commit above)? " SHOULD_COMMIT
+
+if [ "$SHOULD_COMMIT" != "y" ]; then
+    echo "Aborting"
+    exit 1
+fi
+
+if [ -n "$(git status --porcelain)" ]; then
+    git add "$PROJECT_JSON"
+    git commit -m "Prepare for $VERSION"
+fi
+
+pushd MaxMind.MinFraud
+
+dotnet restore
+dotnet build -c Release
+dotnet pack -c Release
+
+popd
+
+pushd MaxMind.MinFraud.UnitTest
+
+dotnet restore
+dotnet build
+# Disabled until the NUnit test runner works better on Linux
+# dotnet run -c Release
+
+popd
+
+read -e -p "Continue given test results? " SHOULD_CONTINUE
+
+if [ "$SHOULD_CONTINUE" != "y" ]; then
+    echo "Aborting"
     exit 1
 fi
 
 if [ ! -d .gh-pages ]; then
     echo "Checking out gh-pages in .gh-pages"
     git clone -b gh-pages git@github.com:maxmind/minfraud-api-dotnet.git .gh-pages
-    cd .gh-pages
+    pushd .gh-pages
 else
     echo "Updating .gh-pages"
-    cd .gh-pages
+    pushd .gh-pages
     git pull
 fi
 
@@ -35,10 +81,10 @@ if [ -n "$(git status --porcelain)" ]; then
     exit 1
 fi
 
-cd ..
+popd
 
 PAGE=.gh-pages/index.md
-cat <<EOF > $PAGE
+cat <<EOF > "$PAGE"
 ---
 layout: default
 title: MaxMind minFraud Score and Insights .NET API
@@ -48,15 +94,12 @@ version: $TAG
 
 EOF
 
-cat README.md >> $PAGE
+cat README.md >> "$PAGE"
 
-nuget restore MaxMind.MinFraud.sln
+monodocer -assembly:MaxMind.MinFraud/bin/Release/net45/MaxMind.MinFraud.dll -importslashdoc:MaxMind.MinFraud/bin/Release/net45/MaxMind.MinFraud.xml "-path:/tmp/dotnet-$TAG" -pretty
+mdoc export-html -o ".gh-pages/doc/$TAG" "/tmp/dotnet-$TAG"
 
-xbuild /p:TargetFrameworkVersion="v4.5" /property:Configuration=Release
-monodocer -assembly:MaxMind.MinFraud/bin/Release/MaxMind.MinFraud.dll -importslashdoc:MaxMind.MinFraud/bin/Release/MaxMind.MinFraud.xml -path:/tmp/mf-dotnet-$TAG -pretty
-mdoc export-html -o .gh-pages/doc/$TAG /tmp/mf-dotnet-$TAG
-
-cd .gh-pages
+pushd .gh-pages
 
 git add doc/
 git commit -m "Updated for $TAG" -a
@@ -70,7 +113,9 @@ fi
 
 git push
 
-cd ..
-git tag $TAG
+popd
+git tag "$TAG"
 git push
 git push --tags
+
+nuget push "MaxMind.MinFraud/bin/Release/MaxMind.MinFraud.$VERSION.nupkg"
