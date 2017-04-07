c5ee4c15400452783612297df3106d4c6ec7522f
Gregory Oschwald <oschwald@gmail.com>
2017-04-07 15:40:55 -0700

    Update CI config

--- dev-bin/build.sh
+++ dev-bin/build.sh
@@ -8,24 +8,28 @@ if [ -n "${DOTNETCORE:-}" ]; then
 
   echo Using .NET CLI
 
-  dotnet restore
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    # This is due to: https://github.com/NuGet/Home/issues/2163#issue-135917905
+    echo "current ulimit is: `ulimit -n`..."
+    ulimit -n 1024
+    echo "new limit: `ulimit -n`"
+  fi
+
+  dotnet restore ./MaxMind.MinFraud.sln
 
   # Running Unit Tests
-  dotnet test -f netcoreapp1.0 -c "$CONFIGURATION" ./MaxMind.MinFraud.UnitTest
+  dotnet test -f netcoreapp1.0 -c "$CONFIGURATION" ./MaxMind.MinFraud.UnitTest/MaxMind.MinFraud.UnitTest.csproj
 
 else
 
   echo Using Mono
 
-  pushd mono
-
   nuget restore
 
-  xbuild /p:Configuration=$CONFIGURATION
+  xbuild /p:Configuration=$CONFIGURATION mono/MaxMind.MinFraud.sln
 
-  mono packages/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe --where "cat != BreaksMono" ./bin/$CONFIGURATION/MaxMind.MinFraud.UnitTest.dll
-
-  popd
+  mono ./packages/xunit.runner.console.2.2.0/tools/xunit.console.exe ./mono/bin/$CONFIGURATION/MaxMind.MinFraud.UnitTest.dll
+  
 fi
 
 popd
