edb3e7ba80070309cec9be6fcf38112cc6dd26af
Gregory Oschwald <oschwald@gmail.com>
2017-10-27 15:29:07 -0700

    Update GeoIP2 dep for new anonymizer outputs

--- MaxMind.MinFraud.UnitTest/Response/IPAddressTest.cs
+++ MaxMind.MinFraud.UnitTest/Response/IPAddressTest.cs
@@ -15,12 +15,27 @@ namespace MaxMind.MinFraud.UnitTest.Response
             {
                 {"risk", 99},
                 {"country", new JObject {{"is_high_risk", true}}},
-                {"location", new JObject {{"local_time", time}}}
+                {"location", new JObject {{"local_time", time}}},
+                {
+                    "traits", new JObject
+                    {
+                        {"is_anonymous", true},
+                        {"is_anonymous_vpn", true},
+                        {"is_hosting_provider", true},
+                        {"is_public_proxy", true},
+                        {"is_tor_exit_node", true},
+                    }
+                }
             }.ToObject<IPAddress>();
 
             Assert.Equal(99, address.Risk);
             Assert.Equal(time, address.Location.LocalTime?.ToString("yyyy-MM-ddTHH:mm:ssK"));
             Assert.True(address.Country.IsHighRisk);
+            Assert.True(address.Traits.IsAnonymous);
+            Assert.True(address.Traits.IsAnonymousVpn);
+            Assert.True(address.Traits.IsHostingProvider);
+            Assert.True(address.Traits.IsPublicProxy);
+            Assert.True(address.Traits.IsTorExitNode);
         }
     }
 }
