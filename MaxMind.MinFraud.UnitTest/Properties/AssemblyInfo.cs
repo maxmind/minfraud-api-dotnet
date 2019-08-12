af39a656e6dbee1b2f9524b90eb94fbb0e7f35d7
Gregory Oschwald <goschwald@maxmind.com>
2019-08-12 08:38:09 -0700

    Bump copyright years

--- MaxMind.MinFraud.UnitTest/Properties/AssemblyInfo.cs
+++ MaxMind.MinFraud.UnitTest/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MaxMind.MinFraud.UnitTest")]
-[assembly: AssemblyCopyright("Copyright ©  2015-2018")]
+[assembly: AssemblyCopyright("Copyright ©  2015-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
