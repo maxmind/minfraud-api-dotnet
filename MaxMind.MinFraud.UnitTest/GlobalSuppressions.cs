15a74516db1f317ee164619796c4d6a92fa20ff6
Gregory Oschwald <oschwald@gmail.com>
2020-09-25 13:29:58 -0700

    Add new suppressions file

---
+++ MaxMind.MinFraud.UnitTest/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Minor Code Smell", "S1905:Redundant casts should not be used", Justification = "<Pending>", Scope = "member", Target = "~M:MaxMind.MinFraud.UnitTest.Request.CustomInputsTest.TestInvalidDouble")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:MaxMind.MinFraud.UnitTest.WebServiceClientTest.TestFullScoreRequest~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:MaxMind.MinFraud.UnitTest.Response.BillingAddressTest.TestBillingAddress")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:MaxMind.MinFraud.UnitTest.WebServiceClientTest.TestFullScoreRequest~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2699:Tests should include assertions", Justification = "<Pending>", Scope = "member", Target = "~M:MaxMind.MinFraud.UnitTest.WebServiceClientTest.TestFullFactorsRequest~System.Threading.Tasks.Task")]
