f3793b21b6761c61318e4f641d0073021abe54e9
Gregory Oschwald <goschwald@maxmind.com>
2021-01-28 14:47:03 -0800

    Move list of code to a more sensible location

--- MaxMind.MinFraud/Response/IPRiskReason.cs
+++ MaxMind.MinFraud/Response/IPRiskReason.cs
@@ -8,51 +8,52 @@ namespace MaxMind.MinFraud.Response
     /// <remarks>
     /// This class provides both a machine-readable code and a human-readable
     /// explanation of the reason for the IP risk score.
-    /// 
-    /// Although more codes may be added in the future, the current codes are:
-    /// 
-    /// <list type="table">
-    /// <item>
-    /// <term>ANONYMOUS_IP</term>
-    /// <description>The IP address belongs to an anonymous network. See the
-    /// object at <c>.IPAddress.Traits</c> for more details.</description>
-    /// </item>
-    /// <item>
-    /// <term>BILLING_POSTAL_VELOCITY</term>
-    /// <description>Many different billing postal codes have been seen on
-    /// this IP address.</description>
-    /// </item>
-    /// <item>
-    /// <term>EMAIL_VELOCITY</term>
-    /// <description>Many different email addresses have been seen on this
-    /// IP address.</description>
-    /// </item>
-    /// <item>
-    /// <term>HIGH_RISK_DEVICE</term>
-    /// <description>A high risk device was seen on this IP address.</description>
-    /// </item>
-    /// <item>
-    /// <term>HIGH_RISK_EMAIL</term>
-    /// <description>A high risk email address was seen on this IP address in
-    /// your past transactions.</description>
-    /// </item>
-    /// <item>
-    /// <term>ISSUER_ID_NUMBER_VELOCITY</term>
-    /// <description>Many different issuer ID numbers have been seen on this
-    /// IP address.</description>
-    /// </item>
-    /// <item>
-    /// <term>MINFRAUD_NETWORK_ACTIVITY</term>
-    /// <description>Suspicious activity has been seen on this IP address
-    /// across minFraud customers.</description>
-    /// </item>
-    /// </list>
     /// </remarks>
     public sealed class IPRiskReason
     {
         /// <summary>
         /// This property is a machine-readable code identifying the reason.
         ///</summary>
+        /// <remarks>
+        /// Although more codes may be added in the future, the current codes are:
+        ///
+        /// <list type="table">
+        /// <item>
+        /// <term>ANONYMOUS_IP</term>
+        /// <description>The IP address belongs to an anonymous network. See the
+        /// object at <c>.IPAddress.Traits</c> for more details.</description>
+        /// </item>
+        /// <item>
+        /// <term>BILLING_POSTAL_VELOCITY</term>
+        /// <description>Many different billing postal codes have been seen on
+        /// this IP address.</description>
+        /// </item>
+        /// <item>
+        /// <term>EMAIL_VELOCITY</term>
+        /// <description>Many different email addresses have been seen on this
+        /// IP address.</description>
+        /// </item>
+        /// <item>
+        /// <term>HIGH_RISK_DEVICE</term>
+        /// <description>A high risk device was seen on this IP address.</description>
+        /// </item>
+        /// <item>
+        /// <term>HIGH_RISK_EMAIL</term>
+        /// <description>A high risk email address was seen on this IP address in
+        /// your past transactions.</description>
+        /// </item>
+        /// <item>
+        /// <term>ISSUER_ID_NUMBER_VELOCITY</term>
+        /// <description>Many different issuer ID numbers have been seen on this
+        /// IP address.</description>
+        /// </item>
+        /// <item>
+        /// <term>MINFRAUD_NETWORK_ACTIVITY</term>
+        /// <description>Suspicious activity has been seen on this IP address
+        /// across minFraud customers.</description>
+        /// </item>
+        /// </list>
+        /// </remarks>
         [JsonPropertyName("code")]
         public string? Code { get; init; }
 
