892f94b6cab6dd505b09d1da304f3297d66e8f2a
Gregory Oschwald <oschwald@gmail.com>
2025-02-10 10:33:52 -0800

    Update release year

--- MaxMind.MinFraud/Properties/AssemblyInfo.cs
+++ MaxMind.MinFraud/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MaxMind, Inc.")]
 [assembly: AssemblyProduct("MaxMind.MinFraud")]
-[assembly: AssemblyCopyright("Copyright ©  2015-2024")]
+[assembly: AssemblyCopyright("Copyright © 2015-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
